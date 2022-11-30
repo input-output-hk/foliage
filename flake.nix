83405a1467da23029e53bd7e89ebf3456351e807
Andrea Bedini <andrea@andreabedini.com>
2022-11-30 11:22:32 +0800

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "Foliage is a tool to create custom Haskell package repositories, in a fully reproducible way.";
+  description =
+    "Foliage is a tool to create custom Haskell package repositories, in a fully reproducible way.";
 
   inputs = {
     nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
@@ -9,39 +10,35 @@
 
   outputs = { self, nixpkgs, flake-utils, haskell-nix }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; inherit (haskell-nix) config; overlays = [haskell-nix.overlay]; };
-      project = pkgs.haskell-nix.cabalProject {
-        src = ./.;
-        compiler-nix-name = "ghc8107";
-        shell.tools = {
-          cabal = {};
-          hlint = {};
-          haskell-language-server = {};
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (haskell-nix) config;
+          overlays = [ haskell-nix.overlay ];
         };
-        shell.buildInputs = with pkgs; [
-          nixpkgs-fmt
-          fsatrace
-        ];
-        modules = [{
-          packages.foliage.components.exes.foliage.dontStrip = false;
-        }];
-      };
-    in {
-      packages.default = project.foliage.components.exes.foliage;
-      devShell = pkgs.mkShell {
-        name = "foliage-dev-shell";
-        buildInputs = with pkgs; [
-          fsatrace
-        ];
-      };
-    });
+        project = pkgs.haskell-nix.cabalProject {
+          src = ./.;
+          compiler-nix-name = "ghc8107";
+          shell.tools = {
+            cabal = { };
+            hlint = { };
+            haskell-language-server = { };
+          };
+          shell.buildInputs = with pkgs; [ nixpkgs-fmt fsatrace ];
+          modules =
+            [{ packages.foliage.components.exes.foliage.dontStrip = false; }];
+        };
+      in {
+        packages.default = project.foliage.components.exes.foliage;
+        devShell = pkgs.mkShell {
+          name = "foliage-dev-shell";
+          buildInputs = with pkgs; [ fsatrace ];
+        };
+      });
 
   nixConfig = {
-    extra-substituters = [
-      "https://cache.iog.io"
-      "https://foliage.cachix.org"
-    ];
+    extra-substituters =
+      [ "https://cache.iog.io" "https://foliage.cachix.org" ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "foliage.cachix.org-1:kAFyYLnk8JcRURWReWZCatM9v3Rk24F5wNMpEj14Q/g="
